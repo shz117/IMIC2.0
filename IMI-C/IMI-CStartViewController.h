15c774c83d6bc04b083065468467096c9c4c487a
Jeremy H. Shi <shz6621091@hotmail.com>
2013-07-26 09:25:10 -0400

    first comm

---
+++ IMI-C/IMI-CStartViewController.h
@@ -0,0 +1,18 @@
+//
+//  IMI-CStartViewController.h
+//  IMI-C
+//
+//  Created by Jiankai Dang on 9/25/12.
+//  Copyright (c) 2012 Jiankai Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MessageUI/MFMailComposeViewController.h>
+#import <MessageUI/MessageUI.h>
+#import <Dropbox/Dropbox.h>
+//#import "IMI_CDataViewController.h"
+
+
+@interface IMI_CStartViewController : UIViewController
+
+@end
